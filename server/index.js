a66ffd4dd22d89f785e19050e3a977ff81aa51dc
Amir <amirhamza27940@gmail.com>
2024-10-30 09:34:01 +0600

    reedit

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT,()=>{
-    console.log(`server is running at http://localhost:${PORT}`)
+    console.log(`server is running at ${PORT}`)
 })
