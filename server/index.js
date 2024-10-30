cb3dc614938f4f4322e256d3c321b329bbbbacec
Amir <amirhamza27940@gmail.com>
2024-10-30 10:04:42 +0600

    edit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 const PORT = process.env.PORT || 4000;
-const host = '0.0.0.0';
+const host = '127.0.0.1:';
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://${host}:${PORT}`);
